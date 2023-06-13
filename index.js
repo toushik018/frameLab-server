a1cfe78d25ab76045be27fe6b0b69e47def8e025
Toushik <tayebhossain018@gmail.com>
2023-06-14 03:24:54 +0600

    made some apis and implemented jwt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config()
 
@@ -10,8 +11,28 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+
+    }
+
+    // bearer token
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qesst1e.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,8 +49,97 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const usersCollection = client.db("frameLabDB").collection("users");
         const classesCollection = client.db("frameLabDB").collection("classes");
         const selectedClassesCollection = client.db("frameLabDB").collection("selectedClasses");
+
+
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+                expiresIn: '1h'
+            })
+            res.send({ token })
+        })
+
+        // users api
+
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result)
+
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'User already exist' })
+            }
+
+            const result = await usersCollection.insertOne(user);
+            res.send(result)
+        })
+
+
+
+        // making admin
+
+
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+
+            // if (req.decoded.email !== email) {
+            //     res.send({ admin: false })
+            // }
+
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' }
+            res.send(result);
+        })
+
+        // instructor
+
+        app.get('/users/instructor/:email', async (req, res) => {
+            const email = req.params.email;
+          
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            const result = { instructor: user?.role === 'instructor' }
+            res.send(result);
+          });
+          
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+
+                $set: {
+                    role: 'admin'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
+
+        // Making Instructor
+
+        app.patch('/users/instructor/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+
+                $set: {
+                    role: 'instructor'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
 
 
         // Create a new class
@@ -58,18 +168,37 @@ async function run() {
         // Classes
 
         app.get('/classes', async (req, res) => {
-            try {
-
-                const classes = await classesCollection.find().toArray();
-                res.status(200).json(classes);
-            } catch (error) {
-                console.error('Error fetching classes:', error);
-                res.status(500).json({ error: 'Internal server error' });
+            const email = req.query.email;
+            if (!email) {
+                res.send([]);
             }
-        });
+            // const decodedEmail = req.decoded.email;
+            // if (email !== decodedEmail) {
+            //     return res.status(403).send({ error: true, message: 'forbidden access' })
+
+            // }
+
+            const query = { instructorEmail: email };
+            const result = await classesCollection.find(query).toArray();
+            res.send(result);
+
+        })
 
 
         //Selected Classes
+
+        app.get('/selectedClasses', async (req, res) => {
+            const email = req.query.email;
+            if (!email) {
+                res.send([]);
+            }
+
+            const query = { email: email };
+            const result = await selectedClassesCollection.find(query).toArray();
+            res.send(result);
+
+        })
+
 
         app.post('/selectedClasses', async (req, res) => {
             const item = req.body;
@@ -79,10 +208,20 @@ async function run() {
             res.send(result)
         })
 
+        // Delete
+
+        app.delete('/selectedClasses/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+
+            const result = await selectedClassesCollection.deleteOne(query);
+            res.send(result)
+        })
+
 
         // Making admin
 
-        app.patch('classes/admin/:id', async (req, res) => {
+        app.patch('/classes/admin/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new Object(id) };
 
